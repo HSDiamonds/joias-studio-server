74dd8ab0911a011e82e3f2b38e580adb0b0b768f
HSDiamonds <heltonsilva@live.com>
2026-04-19 01:04:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const upload = multer();
 
 app.use(cors());
 
-app.post('/remove-bg', upload.single('image_file'), async (req, res) => {
+app.get('/', (req, res) => {
+  res.send('Jóias Studio Server funcionando!');
+});
+
+app.post('/remove-bg', upload.single('image'), async (req, res) => {
   try {
     const form = new FormData();
     form.append('image_file', req.file.buffer, {
@@ -18,22 +22,29 @@ app.post('/remove-bg', upload.single('image_file'), async (req, res) => {
     });
     form.append('size', 'auto');
 
-    const response = await fetch('https://api.remove.bg/v1.0/removebg', {
+    const response = await fetch('https://background-removal-ai.p.rapidapi.com/remove-background', {
       method: 'POST',
       headers: {
-        'X-Api-Key': 'n7Y6GWmkc69D3szpzye7iP7j',
+        'x-rapidapi-host': 'background-removal-ai.p.rapidapi.com',
+        'x-rapidapi-key': '066563ff90mshfb600f9a67a203bp1d1177jsnee39d8700fa9',
         ...form.getHeaders()
       },
       body: form
     });
 
+    if (!response.ok) {
+      const err = await response.text();
+      return res.status(500).json({ error: err });
+    }
+
     const buffer = await response.buffer();
     res.set('Content-Type', 'image/png');
     res.send(buffer);
+
   } catch(e) {
     res.status(500).json({ error: e.message });
   }
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`));
